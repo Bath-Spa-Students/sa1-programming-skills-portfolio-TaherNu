aadd66c79d771f60ddf8d004f7165f61a553bd6a
Taher <Taherabdullah1224@gmail.com>
2024-10-22 17:39:56 +0400

    Biography
    Exercise-3 : Biography

--- Exercise 03/Biography.py
+++ Exercise 03/Biography.py
@@ -1,9 +1,25 @@
-First_Name = str(input ("First Name: ")) #This is a String Variable with an input function which asks the User for their First Name
-Hometown = str(input ("Hometown: "))    #This is a String Variable with an input function which asks the user for their Hometown
-Age = int(input ("Age: ")) #This is an Integer Variable with an input function which asks the user for their Age
-User_Details = First_Name + Hometown,  str(Age) 
-'''This is a String Variable that contains the Variables for the User input details
-The Integer Variable Age was converted to string in this statement as integers cannot be added with strings'''
+"""Exercise 3: Biography - 25 Marks
+In this exercise, you’ll create a program that stores and prints your name, hometown,
+and age to the console using a Python dictionary.
+Steps:
+1. Store the information (name, hometown, and age) as key-value pairs in a
+dictionary.
+2. Print the values on separate lines using a single print() statement.
+3. Use variables with appropriate data types for each piece of information.
+Advanced Requirements:
+Have the user input their name, hometown, and age instead of hardcoding the
+values. Try giving both your first and second name when asked for your name.
+What happens? How can you handle multiple words in Python? Test the
+program by entering a string value for age (e.g., “twenty”). What happens?
+How can you prevent this issue?"""
+#Answer to Exercise 3: Biography
+
+#This is a String Variable with an input function which asks the User for their First Name
+First_Name = str(input ("First Name: "))
+#This is a String Variable with an input function which asks the user for their Hometown 
+Hometown = str(input ("Hometown: "))
+ #This is an Integer Variable with an input function which asks the user for their Age
+Age = int(input ("Age: "))
 
 print(f"User First Name: " + First_Name + "\n" +
       "User Age: " + str(Age) + "\n" +
