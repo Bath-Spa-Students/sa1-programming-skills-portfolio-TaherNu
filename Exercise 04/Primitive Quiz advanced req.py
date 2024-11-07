e218f2ac5fc9325d2192f6f3a4cc11d49c968649
Taher <Taherabdullah1224@gmail.com>
2024-11-07 11:20:32 +0400

    Primitive Quiz advanced req.py

--- Exercise 04/Primitive Quiz advanced req.py
+++ Exercise 04/Primitive Quiz advanced req.py
@@ -1,6 +1,6 @@
 #Exercise 4: Primitive Quiz
 """
-Advanced Requirements:
+Advanced Requirements:.
 Ignore Capitalization: Modify your program to accept answers regardless of
 the capitalization (e.g., “paris”, “Paris”, and “PaRis” should all be considered
 correct). Multiple Questions: Extend the program into a quiz that asks for the
