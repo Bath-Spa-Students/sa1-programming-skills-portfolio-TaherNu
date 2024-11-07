629396e289579216bfd0a801f7c451686c93f49e
Taher <Taherabdullah1224@gmail.com>
2024-11-07 11:12:20 +0400

    Exercise-7
    Exercise-7 Some Counting basic requirements

---
+++ Exercise 04/Primitive Quiz advanced req.py
@@ -0,0 +1,74 @@
+#Exercise 4: Primitive Quiz
+"""
+Advanced Requirements:
+Ignore Capitalization: Modify your program to accept answers regardless of
+the capitalization (e.g., “paris”, “Paris”, and “PaRis” should all be considered
+correct). Multiple Questions: Extend the program into a quiz that asks for the
+capitals of 10 European countries. Provide feedback for each question.
+"""
+# The answer to the question for this exercise starts here:
+
+#input method is used here to ask for user input regarding the question, \n is used to create a new line before the question
+Capital_France = input('\nWhat is the capital of France? ') 
+#if statement is used here to compare if user input is equal to the variables value or not
+if Capital_France.strip().title() == 'Paris':
+#if it is equal to the variables value; it will print the following statement:
+        print(f'Correct the answer is',Capital_France.strip().title(), "!!!")
+#if not it will print the following statement:
+else: 
+        print(f"Your answer is incorrect")
+
+Capital_germany = input('\nWhat is the capital of Germany? ')
+if Capital_germany.strip().lower() == 'berlin':
+        print(f'Correct the answer is',Capital_germany.strip().title(), '!!!')
+else: 
+        print(f"You're answer is incorrect")
+
+Switzerland_Capital = input("\nWhat is the capital of Switzerland? ")
+if Switzerland_Capital.strip().upper() == "BERN" :
+        print(f'Correct the capital of Switzerland is', Switzerland_Capital.strip().title(), '!!!')
+else :
+        print(f"Your answer is incorrect")
+
+Capital_Netherlands = input('\nWhat is the capital of Netherlands? ')
+if Capital_Netherlands.strip().title() == 'Amsterdam' :
+        print(f'Correct the capital of Netherlands is', Capital_Netherlands.strip().title(), '!!!')
+else:
+        print(f'Your answer is incorrect')
+
+Capital_Ukraine = input("\nWhat is the capital of Ukraine? ")
+if Capital_Ukraine.strip().title() == 'Kiev' or 'Kyiv' :
+        print(f"correct the answer is", Capital_Ukraine.strip().title(), '!!!')
+else:
+        print(f'The answer is incorrect')
+
+Capital_Portugal = input('\nWhat is the capital of Portugal? ')
+if Capital_Portugal.strip().title() == 'Lisbon' :
+        print(f'Correct the capial of portugal is ', Capital_Portugal.strip().title(), '!!!')
+else:
+        print(f'Your answer is incorrect')
+
+Capital_Malta = input('\n What is the capital of Malta? ')
+if Capital_Malta.strip().title() == 'Valleta' :
+        print(f'Your answer is correct, The capital of Malta is ', Capital_Malta.strip().title(), '!!!')
+else: 
+        print(f"Your answer is incorrect ")
+        
+Capital_Türkiye = input('What is the capital of Türkiye? ')
+if Capital_Türkiye.strip().title() == 'Ankara' :
+        print(f'Correct the capital of Türkiye is ', Capital_Türkiye.strip().title(), '!!!')
+else: 
+        print(f'Your answer is incorrect')
+
+Capital_Romania = input('\nWhat is the capital of Romania? ')
+if Capital_Romania == 'Bucharest' :
+        print(f"Correct the capital of Romania is ", Capital_Romania.strip().title(), '!!!')
+else:
+        print(f'Your answer is incorrect')
+
+Capital_Georgia = input('What is the capital of Georgia? ')
+if Capital_germany.strip().title() == 'Atlanta' :
+        print('correct the capital of Gerogia is ', Capital_Georgia.strip().title(), '!!!')
+else:
+        print('Your answer is incorrect')
+        
---
+++ Exercise 07/Some Counting.py
@@ -0,0 +1,29 @@
+'''Use your newly acquired knowledge of the for loop to complete the following
+tasks. Print all values to the console in each case. 
+* Write a loop that counts up from 0 to 50 in increments of 1. 
+* Write a loop that counts down from 50 to 0 in decrements of 1. 
+* Write a loop that counts up from 30 to 50 in increments of 1. 
+* Write a loop that counts down from 50 to 10 in decrements of 2. 
+* Write a loop that counts up from 100 to 200 in increments of 5. 
+You may include all loops in a single project'''
+
+#This is a list comprehension where a loop starts counting from 0 and stops when it reaches 51 in increments of 1
+loop1 = [value for value in range(0, 51, 1)]
+#This is a print statement that prints the loop above on a new line
+print(f'\n',loop1)
+
+#This is a list comprehension where a loop starts counting from 50 and stops when it reaches -1 in decrements of 1
+loop2 = [value for value in range(50, -1, -1)]
+print(f'\n',loop2)
+
+#This is a list comprehension where a loop starts counting from 30 and stops when it reaches 51 in increments of 1
+loop3 = [value for value in range(30, 51, 1)]
+print(f'\n',loop3)
+
+#This is a list comprehension where a loop starts counting from 50 and stops when it reaches 9 in decrements of 2
+loop4 = [value for value in range(50, 9, -2)]
+print(f'\n',loop4)
+
+#This is a list comprehension where a loop starts counting from 100 and stops when it reaches 201 in increments of 5
+loop5 = [value for value in range(100, 201, 5)]
+print(f'\n',loop5)
