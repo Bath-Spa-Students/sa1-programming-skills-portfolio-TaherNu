82d26f570823c87780faf70f50a4eafa4456664c
Taher <Taherabdullah1224@gmail.com>
2024-11-08 16:34:40 +0400

    monthsadvanced.py
    Advanced requirements for exercise 5

--- Exercise 05/monthsadvanced.py
+++ Exercise 05/monthsadvanced.py
@@ -1,3 +1,8 @@
+'''Advanced Requirement:
+Leap Year Adjustment: Modify the program to account for leap years. For
+February, ask the user if the year is a leap year and adjust the number of days
+accordingly.'''
+#Dictionary of month numbers and month days
 days_of_month = {1: 31, 
                  2: 28,
                  3: 31,
@@ -10,17 +15,27 @@ days_of_month = {1: 31,
                  10: 31,
                  11: 30,
                  12: 31}
+#days in february for leap year
 leap_Year_days = 29
+#Variable asks user for month number
 month = int(input('Month number?')) 
+#if statement to check if number is valid
 if month < 1 or month > 12:
+    #if invalid and error message is printed
     print('this number of month is invalid, enter a number between 1 and 12')
 else: 
+    #if month is 2 
     if month == 2:
+        #asks if its leap year or no
         year = str(input('Is it a leap year? '))
         if year.strip().lower() == "yes":
+        #if yes print leap month and days
             print('Month', month, 'has', leap_Year_days)
             if year.strip().lower() == "no":
+        #if no print month and days
                 print(f'Month ', month, 'has', days_of_month[month], 'days')
+    #else statement
     else: 
             print(f'Month', month , 'has', days_of_month[month], 'days')
+        #prints the month number and the days
     
