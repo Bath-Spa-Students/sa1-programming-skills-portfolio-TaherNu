2952506a5f40f71a990d4098b3239ad0a8be7f99
Taher <Taherabdullah1224@gmail.com>
2024-11-08 16:34:18 +0400

    Exercise 05 - Months
    basic requirements for exercise-05 months

--- Exercise 05/months.py
+++ Exercise 05/months.py
@@ -10,7 +10,7 @@ and the values are the number of days in those months.
 and print the number of days in the corresponding month.'''
 #Answer to Exercise 5: Days of the Month :
 
-# this is a dictionary storing the month numbers and the number of days within each one
+#Dictionary of month numbers and month days
 days_of_month = {1: 31, 
                  2: 28,
                  3: 31,
@@ -23,14 +23,14 @@ days_of_month = {1: 31,
                  10: 31,
                  11: 30,
                  12: 31} 
-#This is an integer variable which asks the user what month number they want
+#Variable asks user for month number
 month = int(input('Month number? ')) 
-#This if statement checks if the number inputted is less than 1 or more than 12
+#if statement to check if number is valid
 if month < 1 or month > 12:
-#if the number inputted is less than 1 or more than 12 the following message is printed
+#if invalid and error message is printed
     print('this number of month is invalid, enter a number between 1 and 12')
 else:
-#If the number inputted is not less than 1 or more than 12 the following is printed
+#else statement
     print(f'month number ', month , 'has', days_of_month[month], 'days')
-"""this print statement finds the number inputted and prints it 
-then finds its corresponding days of the month in the dictionary and prints it"""
+#prints the month number and the days
+
