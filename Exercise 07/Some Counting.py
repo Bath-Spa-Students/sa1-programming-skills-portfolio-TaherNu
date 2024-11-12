fb16cabcceebb3dd8fc276ec41518cbad5082796
Taher <Taherabdullah1224@gmail.com>
2024-11-12 11:51:10 +0400

    Some Counting.py
    exercise 7: some counting

--- Exercise 07/Some Counting.py
+++ Exercise 07/Some Counting.py
@@ -7,23 +7,28 @@ tasks. Print all values to the console in each case.
 * Write a loop that counts up from 100 to 200 in increments of 5. 
 You may include all loops in a single project'''
 
-#This is a list comprehension where a loop starts counting from 0 and stops when it reaches 51 in increments of 1
+#ANSWER:
+#list comprehension generates numbers from 0-50 in increments of 1
 loop1 = [value for value in range(0, 51, 1)]
-#This is a print statement that prints the loop above on a new line
+#Prints generated list
 print(f'\n',loop1)
 
-#This is a list comprehension where a loop starts counting from 50 and stops when it reaches -1 in decrements of 1
+#list comprehension generates numbers from 50-0 in decements of -1
 loop2 = [value for value in range(50, -1, -1)]
+#Prints generated list
 print(f'\n',loop2)
 
-#This is a list comprehension where a loop starts counting from 30 and stops when it reaches 51 in increments of 1
+#list comprehension generates numbers from 30-50 in increments of 1
 loop3 = [value for value in range(30, 51, 1)]
+#Prints generated list
 print(f'\n',loop3)
 
-#This is a list comprehension where a loop starts counting from 50 and stops when it reaches 9 in decrements of 2
+#list comprehension generates numbers from 50-10 in decrements of -2
 loop4 = [value for value in range(50, 9, -2)]
+#Prints generated list
 print(f'\n',loop4)
 
-#This is a list comprehension where a loop starts counting from 100 and stops when it reaches 201 in increments of 5
+#list comprehension generates numbers from 100-200 in increments of 5
 loop5 = [value for value in range(100, 201, 5)]
+#Prints generated list
 print(f'\n',loop5)
