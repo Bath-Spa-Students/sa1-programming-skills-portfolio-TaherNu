13081f49c0b3637cdfe6d762baba8c6dac2f07a7
Taher <Taherabdullah1224@gmail.com>
2024-11-19 12:54:42 +0400

    List-Names
    Bonus exercise: List-Names

---
+++ Z_Bonus Exercises/List -Names.py
@@ -0,0 +1,6 @@
+"""Store the names of a few of your friends in a list called names. 
+Print each person’s name by accessing each element in the list, one at a time."""
+
+names = ['MK', 'shakir', 'ahmed', 'boko', 'josh']
+for name in names:
+    print(name.title())
