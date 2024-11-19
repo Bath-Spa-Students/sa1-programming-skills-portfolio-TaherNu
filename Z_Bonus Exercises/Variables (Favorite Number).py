ba880fed5d753843883693cc2d96d117000a825c
Taher <Taherabdullah1224@gmail.com>
2024-11-19 12:54:09 +0400

    Variables (favorite number)
    Bonus exercise: variables (favouite number)

---
+++ Z_Bonus Exercises/Variables (Favorite Number).py
@@ -0,0 +1,10 @@
+"""Use a variable to represent your favorite number. Then, using that variable, 
+create a message that reveals your favorite number. Print that message."""
+'''Answer for exercise: '''
+
+#int variable
+fav_num = 5
+#message
+message = f"my favourite number is {fav_num}"
+#print message
+print(message)
