78778eb9b1a230a1f70f29c424dd0d534f38bd01
Taher <Taherabdullah1224@gmail.com>
2024-11-20 13:39:32 +0400

    String Concatenation
    Bonus exercise - String Concatenation

---
+++ Z_Bonus Exercises/Strings Concatenation.py
@@ -0,0 +1,6 @@
+"""Write three strings in different variables and print the output as one string."""
+v1 = 'hi, '
+v2 = 'we should hang out soon, '
+v3 = 'let me know when your free'
+
+print(v1.title()+ v2+ v3)
