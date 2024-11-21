a1221b2ee0b773efafd3a81e8cae4c0a758ede9a
Taher <Taherabdullah1224@gmail.com>
2024-11-21 12:13:41 +0400

    Biography Adv Req

--- Exercise 03/Biography Advanced Req.py
+++ Exercise 03/Biography Advanced Req.py
@@ -13,9 +13,11 @@ First_Name = str(input ("First Name: "))
 Hometown = str(input ("Hometown: "))
  #This is an Integer Variable with an input function which asks the user for their Age
 Age = int(input ("Age: "))
+info = {"First name:" :First_Name,
+        "Hometown:" :Hometown,
+        "Age:" : Age}
+
+print(f"Name: {info['First name:'].title().strip()}\nHometown: {info['Hometown:'].title().strip()} \nAge: {info['Age:']}")
 #The Integer Variable Age was converted to string here as string concatenation cannot be used with integars 
-print(f"User First Name: " + First_Name.strip().title() + "\n" +
-      "User Age: " + str(Age).strip() + "\n" +
-      "User Hometown: " + Hometown.strip().title() ) 
-'''This print statement uses string concatenation to put all the String Variables into one singular line 
-"\n" is used to create a new line after each Variable'''
+
+'''\n is used to create a new line after each Variable'''
