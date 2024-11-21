eb23078751fdfac2b40ac8ccae35ad03ec25d459
Taher <Taherabdullah1224@gmail.com>
2024-11-21 12:13:56 +0400

    Biography
    exercise 3 - biography

--- Exercise 03/Biography.py
+++ Exercise 03/Biography.py
@@ -7,12 +7,10 @@ dictionary.
 2. Print the values on separate lines using a single print() statement.
 3. Use variables with appropriate data types for each piece of information.
 """
-#This name variable is associated with a string value data type
-name = str('Taher Abdalla')
-#This hometown variable is associated with a string value data type
-hometown = str('Sharjah')
-#This age variable is associated with an integar value data type
-age = int(17)
+
+info = {"Name" : "Taher Abdalla",
+        "Hometown": "Sharjah",
+        "age": "17"}
 #this print statement prints the variables above on multiple lines using '\n', and string concatenation
-print(f"Name: " + name.title() + '\n' + "Hometown: " + hometown.title() + "\n" + 'Age:' + str(age))
+print(f"Name: {info['Name']}\nHometown: {info['Hometown']}\nAge: {info['age']}")
 #The variable age has been converted into a string as string concatenation cannot be used with integar values
